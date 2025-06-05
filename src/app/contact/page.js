b662ab14074991e0b05bed61f21092c50c4a4536
HAN-PIN <n01747642@humber.ca>
2025-06-05 16:00:36 -0400

    Edit some contents and add a sentence in home page.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,7 +4,7 @@ import ContactForm from '../ContactForm';
 export default function Contact() {
   return (
     <Layout1>
-      <h1>Contact Us</h1>
+      <h1>Contact Me</h1>
       <ContactForm />
     </Layout1>
   );
