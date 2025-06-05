1604e04df62e6e5fc80290eb13b5bfb8456f6db1
HAN-PIN <n01747642@humber.ca>
2025-06-05 17:04:16 -0400

    Create a ScreeShot folder and chage the color of h2 words

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Layout1>
       <h1>Welcome to React Assignment</h1>
-      <h2>This is my first individual-assignment-1</h2>
+      <h2 style={{ color: 'gray'}}>This is my first individual-assignment-1</h2>
       <Counter />
     </Layout1>
   );
